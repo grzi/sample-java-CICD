b3426740ee79068e6efde0d1399e70634a5046b2
Jérémy Thulliez <43185161+zthulj@users.noreply.github.com>
2019-10-21 22:44:56 +0200

    minor

--- src/main/java/com/github/zthulj/Main.java
+++ src/main/java/com/github/zthulj/Main.java
@@ -2,7 +2,7 @@ package com.github.zthulj;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello cicd for the 59 time");
+        System.out.println("Hello cicd for the 60 time");
     }
 }
 
